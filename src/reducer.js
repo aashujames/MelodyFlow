62e487cd6ade725c746ac10612a9e33e9d31bf2c
anubhav-sl <anubhav@steinnlabs.com>
2022-04-26 22:41:06 +0530

    ContextApi, Using dataLayer, using reducer to set state. Added DataLayer, Reducer, Body, Footer, Sidebar files.

---
+++ src/reducer.js
@@ -0,0 +1,29 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    token: "BQAmGWJIhcwkrrRYxyGXqsuqKWhTRHVULnAquw6LzgtUWHMZ_1Bc4Hg05a3xCOigO0vAftq59S-zcNGhQNYRQDyFnAm41wfSPq4t8K1W6Z2BrCWFeFHFjuJ-hfx2hoSucvT4Hr-zSZh4Zuva6SUJcwJY4LvWxpbWNU2_cUQZiw_e9GS1yjBr"
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+
+    switch (action.type) {
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user
+            };
+
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.token
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
--- src/spotify.js
+++ src/spotify.js
@@ -4,17 +4,24 @@ const redirectUri = "http://localhost:3000/";
 const clientId = "0fd8164872364b0f8a3c15831a4b8c43";
 
 const scopes = [
-  "user-read-currently-playing",
-  "user-read-recently-played",
-  "user-read-playback-state",
-  "user-top-read",
-  "user-modify-playback-state",
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state"
 ];
 
 export const getTokenFromUrl = () => {
-    return
-}
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+};
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-  "%20"
+    "%20"
 )}&response_type=token&show_dialog=true`;
