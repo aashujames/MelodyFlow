a48cce767814a6c84b64e50a6c9d72db500c41e9
anubhav-sl <anubhav@steinnlabs.com>
2022-04-27 18:30:06 +0530

    Styling of sidebar and added sidebarOption

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Sidebar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I m the sidebar</h1>
+            <img
+                className="sidebar__logo"
+                src="https://e7.pngegg.com/pngimages/341/581/png-clipart-qq-music-music-qq-text-computer.png"
+                alt=""
+            />
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+
+            <br />
+            <strong className="sidebar__title">PLAYLISTS</strong>
+            <hr />
+
+            <SidebarOption title="Hip hop" />
+            <SidebarOption title="Rock" />
+            <SidebarOption title="RnB" />
         </div>
     );
 }
