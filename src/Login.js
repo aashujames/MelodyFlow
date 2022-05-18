777c1afa4d9f7e57c11980282f8a54bbfd09cb32
Ashish Gupta <ashishgupt25@gmail.com>
2022-05-18 23:05:42 +0530

    Added songs row

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function Login() {
     return (
         <div className="login">
             <img
-                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                src="https://e7.pngegg.com/pngimages/341/581/png-clipart-qq-music-music-qq-text-computer.png"
                 alt=""
             />
 
