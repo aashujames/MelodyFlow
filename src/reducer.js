6841ee0189bd08f7da9b2221c04d5356d4ee4372
Ashish Gupta <ashishgupt25@gmail.com>
2022-05-31 21:32:36 +0530

    initial state changed

--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,8 @@ export const initialState = {
     user: null,
     playlists: [],
     playing: false,
-    item: null
-    // token: "BQAmGWJIhcwkrrRYxyGXqsuqKWhTRHVULnAquw6LzgtUWHMZ_1Bc4Hg05a3xCOigO0vAftq59S-zcNGhQNYRQDyFnAm41wfSPq4t8K1W6Z2BrCWFeFHFjuJ-hfx2hoSucvT4Hr-zSZh4Zuva6SUJcwJY4LvWxpbWNU2_cUQZiw_e9GS1yjBr"
+    item: null,
+    token: ""
 };
 
 const reducer = (state, action) => {
